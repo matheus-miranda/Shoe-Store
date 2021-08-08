22311a85b38863b8b6f005d3318f1a316f491579
matheus-miranda <matheus.sm86@gmail.com>
2021-08-08 19:57:04 -0300

    Get list of shoes and observe in ShoeListFragment

--- app/src/main/java/com/udacity/shoestore/ui/ShoeListFragment.kt
+++ app/src/main/java/com/udacity/shoestore/ui/ShoeListFragment.kt
@@ -6,12 +6,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragmentShoeListBinding
 
 class ShoeListFragment : Fragment() {
 
     private lateinit var binding: FragmentShoeListBinding
+    private val viewModel by activityViewModels<ShoeListViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,6 +22,9 @@ class ShoeListFragment : Fragment() {
     ): View {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_shoe_list, container, false)
 
+        viewModel.shoeList.observe(viewLifecycleOwner, Observer { shoeList ->
+            // TODO: Observe shoe list and bind to LinearLayout
+        })
 
         return binding.root
     }
--- app/src/main/java/com/udacity/shoestore/ui/ShoeListViewModel.kt
+++ app/src/main/java/com/udacity/shoestore/ui/ShoeListViewModel.kt
@@ -1,6 +1,25 @@
 package com.udacity.shoestore.ui
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.udacity.shoestore.datasource.ShoeDataSource
+import com.udacity.shoestore.model.Shoe
 
 class ShoeListViewModel : ViewModel() {
+
+    private val _shoeList = MutableLiveData<List<Shoe>>()
+    val shoeList: LiveData<List<Shoe>> get() = _shoeList
+
+    init {
+        _shoeList.value = getList()
+    }
+
+    private fun getList(): List<Shoe> {
+        return ShoeDataSource.getList()
+    }
+
+    fun addShoe(shoe: Shoe) {
+        ShoeDataSource.addShoe(shoe)
+    }
 }
