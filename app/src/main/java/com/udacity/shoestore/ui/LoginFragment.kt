327f5434fb179378cd55ea3688a1b55cd9dd61c8
matheus-miranda <matheus.sm86@gmail.com>
2021-08-07 21:55:15 -0300

    Pass the username as argument from Login to Welcome screen

--- app/src/main/java/com/udacity/shoestore/ui/LoginFragment.kt
+++ app/src/main/java/com/udacity/shoestore/ui/LoginFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
@@ -29,11 +30,34 @@ class LoginFragment : Fragment() {
      */
     private fun insertListeners() {
         binding.btnSignup.setOnClickListener {
-            findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToWelcomeFragment())
+            checkFields()
         }
 
         binding.btnLogin.setOnClickListener {
-            findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToWelcomeFragment())
+            checkFields()
         }
+    }
+
+    /**
+     * Check if text fields are empty, if not, navigate to the next screen
+     */
+    private fun checkFields() {
+        if (binding.etEmail.text.isNullOrBlank() || binding.etPassword.text.isNullOrEmpty())
+            Toast.makeText(context, "Please fill out all the fields", Toast.LENGTH_SHORT).show()
+        else
+            navigateToWelcomeScreen(binding.etEmail.text.toString())
+    }
+
+    /**
+     * Navigate to welcome screen and pass in the username
+     * @param: email the e-mail that the user entered
+     */
+    private fun navigateToWelcomeScreen(email: String) {
+        val user = email.substringBefore('@') // Get the string before the @ symbol
+        findNavController().navigate(
+            LoginFragmentDirections.actionLoginFragmentToWelcomeFragment(
+                user
+            )
+        )
     }
 }
