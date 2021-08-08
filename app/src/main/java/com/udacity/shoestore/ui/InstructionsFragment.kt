f5b6a7b3cda2a9f4af30cffb2de3af20e79651d6
matheus-miranda <matheus.sm86@gmail.com>
2021-08-07 22:34:01 -0300

    Create InstructionsFragment, inflate via DataBindingUtil and navigate to it

---
+++ app/src/main/java/com/udacity/shoestore/ui/InstructionsFragment.kt
@@ -0,0 +1,29 @@
+package com.udacity.shoestore.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.FragmentInstructionsBinding
+
+class InstructionsFragment : Fragment() {
+
+    private lateinit var binding: FragmentInstructionsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_instructions,
+            container,
+            false
+        )
+
+        return binding.root
+    }
+}
--- app/src/main/java/com/udacity/shoestore/ui/WelcomeFragment.kt
+++ app/src/main/java/com/udacity/shoestore/ui/WelcomeFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragmentWelcomeBinding
 
@@ -20,9 +21,25 @@ class WelcomeFragment : Fragment() {
     ): View {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_welcome, container, false)
 
-        // Display username on welcome TextView
-        binding.tvWelcome.text = getString(R.string.welcome_user, args.username)
+        initData()
+        insertListeners()
 
         return binding.root
+    }
+
+    /**
+     * Sets onClickListener for button
+     */
+    private fun insertListeners() {
+        binding.btnInstructions.setOnClickListener {
+            findNavController().navigate(WelcomeFragmentDirections.actionWelcomeFragmentToInstructionsFragment())
+        }
+    }
+
+    /**
+     * Initialize data
+     */
+    private fun initData() {
+        binding.tvWelcome.text = getString(R.string.welcome_user, args.username)
     }
 }
