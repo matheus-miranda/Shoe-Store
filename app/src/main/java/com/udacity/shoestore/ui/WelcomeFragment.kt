3f209ddbeed92c3221f385a2934e59106e56e4c3
matheus-miranda <matheus.sm86@gmail.com>
2021-08-07 22:21:01 -0300

    Create WelcomeFragment layout and display username

--- app/src/main/java/com/udacity/shoestore/ui/WelcomeFragment.kt
+++ app/src/main/java/com/udacity/shoestore/ui/WelcomeFragment.kt
@@ -4,16 +4,25 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.FragmentWelcomeBinding
 
 class WelcomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentWelcomeBinding
+    private val args by lazy { WelcomeFragmentArgs.fromBundle(requireArguments()) }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_welcome, container, false)
 
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
+        // Display username on welcome TextView
+        binding.tvWelcome.text = getString(R.string.welcome_user, args.username)
+
+        return binding.root
     }
 }
