7eef64ccb34ae3fe14d441ab71eee8db6205ba9e
matheus-miranda <matheus.sm86@gmail.com>
2021-08-08 15:27:40 -0300

    Create Shoe List screen and navigate to it

--- app/src/main/java/com/udacity/shoestore/ui/InstructionsFragment.kt
+++ app/src/main/java/com/udacity/shoestore/ui/InstructionsFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragmentInstructionsBinding
 
@@ -23,7 +24,14 @@ class InstructionsFragment : Fragment() {
             container,
             false
         )
+        insertListeners()
 
         return binding.root
+    }
+
+    private fun insertListeners() {
+        binding.btnNext.setOnClickListener {
+            findNavController().navigate(InstructionsFragmentDirections.actionInstructionsFragmentToShoeListFragment())
+        }
     }
 }
