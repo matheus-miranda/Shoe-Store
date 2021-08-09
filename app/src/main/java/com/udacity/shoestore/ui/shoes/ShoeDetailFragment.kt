5f638cfef9960da13ff9273f46accfd5194136c3
matheus-miranda <matheus.sm86@gmail.com>
2021-08-08 23:09:18 -0300

    Add cancel button behavior from Shoe Detail

--- app/src/main/java/com/udacity/shoestore/ui/shoes/ShoeDetailFragment.kt
+++ app/src/main/java/com/udacity/shoestore/ui/shoes/ShoeDetailFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragmentShoeDetailBinding
 
@@ -18,6 +19,15 @@ class ShoeDetailFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_shoe_detail, container, false)
+
+        insertListeners()
+
         return binding.root
+    }
+
+    private fun insertListeners() {
+        binding.btnCancel.setOnClickListener {
+            findNavController().navigateUp()
+        }
     }
 }
